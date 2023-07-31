400f6682007a088173e389db75e396d0478994f6
Tarek Aloui <taloui@college.harvard.edu>
2023-07-31 01:01:34 -0400

    added tailwind themes without the correct palette

--- src/components/Appbar/index.tsx
+++ src/components/Appbar/index.tsx
@@ -3,7 +3,7 @@ import ThemeSwitcher from '../ThemeSwitcher'
 
 const Appbar: FC = () => {
 	return (
-		<nav className="flex items-center justify-between bg-red-200 p-6 text-white dark:bg-gray-800">
+		<nav className="flex items-center justify-between bg-primary p-6 text-t-primary">
 			<h1 className="text-4xl">Title</h1>
 			<ThemeSwitcher />
 		</nav>
