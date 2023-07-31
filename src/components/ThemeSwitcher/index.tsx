6d32caaa9f9f937ebc37c54ef13d77dd68decfe0
Tarek Aloui <taloui@college.harvard.edu>
2023-07-31 01:36:52 -0400

    turned theme switcher to moon img-button

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -3,6 +3,12 @@ import { useTheme } from 'next-themes'
 import { useState, useEffect } from 'react'
 
 const ThemeSwitcher = () => {
+	// Constants
+	const lightMoonIcon =
+		'M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z'
+	const darkMoonIcon =
+		'M8.509 5.75c0-1.493.394-2.96 1.144-4.25h-.081a8.5 8.5 0 1 0 7.356 12.746A8.5 8.5 0 0 1 8.509 5.75Z'
+	// States
 	const [mounted, setMounted] = useState(false)
 	const { theme, setTheme } = useTheme()
 
@@ -18,19 +24,23 @@ const ThemeSwitcher = () => {
 
 	return (
 		<div>
-			<label className="relative mr-5 inline-flex cursor-pointer items-center">
-				<input
-					type="checkbox"
-					value={theme}
-					className="peer sr-only"
-					onChange={onChangeThemeToggle}
-					checked={theme === 'dark'}
-				/>
-				<div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-yellow-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-yellow-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-yellow-800"></div>
-				<span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-					{theme}
-				</span>
-			</label>
+			<button onClick={onChangeThemeToggle}>
+				<svg
+					className="h-6 w-6 text-gray-800 dark:text-white"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill={theme == 'light' ? 'currentColor' : 'none'}
+					viewBox="0 0 18 20"
+				>
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d={theme == 'light' ? lightMoonIcon : darkMoonIcon}
+					/>
+				</svg>
+			</button>
 		</div>
 	)
 }
